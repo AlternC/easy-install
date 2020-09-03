f05ab7a4658356ce75d8690001b425b932a61c41
alban <git@albancrommer.com>
2020-09-03 17:53:46 +0200

    [fix] buster requires wget

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ fi;
 misc "Installing mandatory packages"
  
 # Installs various packages required to work
-apt_get apache2 apache2-bin libapache2-mpm-itk dnsutils lsb-release inetutils-ping pwgen gnupg2
+apt_get apache2 apache2-bin libapache2-mpm-itk dnsutils lsb-release inetutils-ping pwgen gnupg2 wget
 
 # Manually disable mpm event
 a2dismod mpm_event 
